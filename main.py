a40ba36b6fdf4bb240c06ffb4a5188748787ab0b
Daxics <61597304+Daxics@users.noreply.github.com>
2022-10-14 00:58:47 +0300

    Make funk

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from PIL import Image
 import glob
 import os
 
-out_dir = ''
-cnt = 0
-for img in glob.glob('C:\\Users\\Turka\\Desktop\\New Folder\\/*.bmp'):
-    Image.open(img).save(os.path.join(out_dir, str(cnt) + '.png'))
-    cnt += 1
+
+def convert(self, file_dir=os.getcwd(), save_dir=os.getcwd(), file_name='/*', file_start_res='jpg', file_end_res='png'):
+    for img in glob.glob(file_dir + file_name + '.' + file_start_res):
+        file_mid_name = os.path.basename(
+            img.removesuffix('.' + file_start_res))
+        Image.open(img).save(os.path.join(
+            save_dir, file_mid_name + '.' + file_end_res))
