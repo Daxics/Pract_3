64f1f4b828b79dd7f1937a9e3fff40b1afc7eaa7
Daxics <61597304+Daxics@users.noreply.github.com>
2022-10-14 00:59:57 +0300

    Return

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ import os
 
 
 def convert(self, file_dir=os.getcwd(), save_dir=os.getcwd(), file_name='/*', file_start_res='jpg', file_end_res='png'):
+    mass = []
     for img in glob.glob(file_dir + file_name + '.' + file_start_res):
         file_mid_name = os.path.basename(
             img.removesuffix('.' + file_start_res))
         Image.open(img).save(os.path.join(
             save_dir, file_mid_name + '.' + file_end_res))
+        mass.append(os.path.basename(
+            str(glob.glob(save_dir + '/' + file_mid_name + '.' + file_end_res)[0])))
+    return mass
